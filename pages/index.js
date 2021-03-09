4e3d69560a3072fdccb0445be47430d6e6c488c6
Sami Ullah <muftisamiullah@localhost.localdomain>
2021-03-09 14:16:33 +0530

    added remove token from db fuctionality

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
-      <h1>CFS WEBSITE changes</h1>
+      <h1>CFS WEBSITE changes by sami</h1>
       <Footer/>
     </>
   )
