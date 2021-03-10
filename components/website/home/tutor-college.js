123f4164f43627ab9e1ec246d9e26ddb8f32a605
Sami Ullah <muftisamiullah@localhost.localdomain>
2021-03-10 18:30:52 +0530

    fixed issue with owl carousal

---
+++ components/website/home/tutor-college.js
@@ -0,0 +1,55 @@
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import dynamic from 'next/dynamic'
+
+const OwlCarousel = dynamic(() => import("react-owl-carousel"), { ssr: false })
+
+export default function TutorCollege(){
+    return(
+        <section className="section brand_icons_bg pt-5 pb-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 text-center mb-5">
+                        <div className="Content_Covered_title pb-3">
+                            <h2>Some of our tutors studied at these colleges:  </h2> 
+                        </div>
+                    </div>
+                    <OwlCarousel items={4} className="owl-carousel" loop autoplay={true} nav margin={10}>
+                        <div className="item">
+                            <div className="brand_icons">
+                                <ul>
+                                    <li><img src="../images/brand-icons/icon1.png" className="img-fluid" alt=""/></li>
+                                    <li><img src="../images/brand-icons/icon5.png" className="img-fluid" alt=""/></li>
+                                </ul>
+                            </div> 
+                        </div>
+                        <div className="item">
+                            <div className="brand_icons">
+                                <ul>
+                                    <li><img src="../images/brand-icons/icon2.png" className="img-fluid" alt=""/></li>
+                                    <li><img src="../images/brand-icons/icon6.png" className="img-fluid" alt=""/></li>
+                                </ul>
+                            </div> 
+                        </div>
+                        <div className="item">
+                            <div className="brand_icons">
+                                <ul>
+                                    <li><img src="../images/brand-icons/icon3.png" className="img-fluid" alt=""/></li>
+                                    <li><img src="../images/brand-icons/icon7.png" className="img-fluid" alt=""/></li>
+                                </ul>
+                            </div> 
+                        </div>
+                        <div className="item">
+                            <div className="brand_icons">
+                                <ul>
+                                <li><img src="../images/brand-icons/icon4.png" className="img-fluid" alt=""/></li>
+                                <li><img src="../images/brand-icons/icon8.png" className="img-fluid" alt=""/></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </OwlCarousel>
+                </div>
+            </div>
+        </section>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 import Head from 'next/head'
-import Header from '../components/website/header'
-import Navbar from '../components/website/navbar'
-import Footer from '../components/website/footer'
-import Banner from '../components/website/banner'
-import ContentCovered from '../components/website/content-covered'
-import FindTbs from '../components/website/find-tbs'
-import AssignmentHelp from '../components/website/get-assignment-help'
-import AskExpert from '../components/website/ask-an-expert'
-import BuySubscription from '../components/website/buy-subscription'
-import SubjectsWeCover from '../components/website/subjects-we-cover'
-import PopularTbs from '../components/website/popular-tbs'
-import SubscribeHere from '../components/website/subscribe-here'
-import TutorCollege from '../components/website/tutor-college'
-import Follow from '../components/website/follow'
+import Header from '../components/website/home/header'
+import Navbar from '../components/website/home/navbar'
+import Footer from '../components/website/home/footer'
+import Banner from '../components/website/home/banner'
+import ContentCovered from '../components/website/home/content-covered'
+import FindTbs from '../components/website/home/find-tbs'
+import AssignmentHelp from '../components/website/home/get-assignment-help'
+import AskExpert from '../components/website/home/ask-an-expert'
+import BuySubscription from '../components/website/home/buy-subscription'
+import SubjectsWeCover from '../components/website/home/subjects-we-cover'
+import PopularTbs from '../components/website/home/popular-tbs'
+import SubscribeHere from '../components/website/home/subscribe-here'
+import TutorCollege from '../components/website/home/tutor-college'
+import Follow from '../components/website/home/follow'
 
 export default function Home() {
   return (
@@ -21,7 +21,7 @@ export default function Home() {
         <title>Crazy For Study</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/> 
-        {/* <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> */}
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
       </Head>
       <Header/>
       <Navbar/>
