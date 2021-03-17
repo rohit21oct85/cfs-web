28e7863389ccbfa722cfbdc9f414897984f54f45
Sami Ullah <muftisamiullah@localhost.localdomain>
2021-03-17 18:27:46 +0530

    created navbar apis and subsubject sp page

--- components/common/pagination.js
+++ components/common/pagination.js
@@ -37,7 +37,7 @@ export default function  Pagination({...props}) {
             <div className="next_prew">
                 <ul>
                     <li><a href="#" className="border-left-0 " onClick={handlePrev}>Previous</a></li>
-                    {/* {[...Array(pages)].map((e, i) => 
+                    {[...Array(pages)].map((e, i) => 
                         ( i!=0 && i<4 ) &&
                             <li key={i}>
                                 <a href="#" className={props.pageNo+1 == i ? 'active':''} onClick={handleClick}>{i}</a>
@@ -50,12 +50,12 @@ export default function  Pagination({...props}) {
                                 <a href="#" className={props.pageNo+1 == i ? 'active':''} onClick={handleClick}>{i}</a>
                                 {console.log(props.pageNo+1, i)}
                             </li>
-                    )} */}
-                    <li><a href="#" className={props.pageNo+1 == 1 ? 'active':''} onClick={handleClick}>1</a></li>
+                    )}
+                    {/* <li><a href="#" className={props.pageNo+1 == 1 ? 'active':''} onClick={handleClick}>1</a></li>
                     <li><a href="#" className={props.pageNo+1 == 2 ? 'active':''} onClick={handleClick}>2</a></li>
                     <li><a href="#" className={props.pageNo+1 == 3 ? 'active':''} onClick={handleClick}>3</a></li>
                     <li><a href="#" className={props.pageNo+1 == 4 ? 'active':''} onClick={handleClick}>4</a></li>
-                    <li><a href="#" className={props.pageNo+1 == 5 ? 'active':''} onClick={handleClick}>5</a></li>
+                    <li><a href="#" className={props.pageNo+1 == 5 ? 'active':''} onClick={handleClick}>5</a></li> */}
                     <li><a href="#" onClick={handleNext}>Next</a></li>
                 </ul>
             </div>
