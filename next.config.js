2507f5a4379932bd61e3c85874238b5fa4a9d515
Sami Ullah <muftisamiullah@localhost.localdomain>
2021-03-17 18:16:14 +0530

    created navbar apis and subsubject sp page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        // HOST:`http://127.0.0.1:8080`
-        HOST:`https://cfs-admin.herokuapp.com`
+        HOST:`http://127.0.0.1:8080`
+        // HOST:`https://cfs-admin.herokuapp.com`
     },
 }
