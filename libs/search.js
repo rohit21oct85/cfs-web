c767f2c7dd9dee211c04bbaa93432c156699ccad
Sami Ullah <muftisamiullah@localhost.localdomain>
2021-03-31 18:44:19 +0530

    added assignment page 2

--- libs/search.js
+++ libs/search.js
@@ -3,10 +3,11 @@ import { apiUrl } from '../config/config'
 
 export async function searchData(params) {
     try {
-        console.log(params);
+        const searchT = params.search ? params.search : params.searchText;
+        console.log(searchT,"params-searchT")
         const res = await axios.all([
-            axios.get(apiUrl + 'books/search-chapter-question/'+params.searchText+'/'+params.limit),
-            axios.get(apiUrl + 'books/search-book-name-isbn/'+params.searchText+'/'+params.limit)
+            axios.get(apiUrl + 'books/search-chapter-question/'+searchT+'/'+params.limit),
+            axios.get(apiUrl + 'books/search-book-name-isbn/'+searchT+'/'+params.limit)
         ])
         return {'data1':res[0].data,'data2':res[1].data};
     }
