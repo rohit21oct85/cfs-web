523dd991141439d7f93135b61ce1b19e8d3edbab
Sami Ullah <muftisamiullah@localhost.localdomain>
2021-04-06 16:12:52 +0530

    added pages for dashboard

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 module.exports = {
     env: {
-        // HOST:`http://127.0.0.1:8080`
-        // HOST:`https://cfs-admin.herokuapp.com`
-        HOST:`http://13.233.125.223:8080`
+        HOST:`http://13.233.125.223:8080`,
+        NEXTAUTH_URL:`http://13.126.229.6`
     },
 }
+        // HOST:`https://cfs-admin.herokuapp.com`
+        // HOST:`http://13.233.125.223:8080`
 // http://13.233.125.223:8080/web/v1/books/view-all
--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -126,7 +126,7 @@ export default function  SignUp({providers}) {
                         <span key={provider.id}>
                         {provider.id === "credentials" ? <span></span> :
                             <li key={provider.name}>
-                                <a href="#" className={`${provider.id}_link`} onClick={(e) => { e.preventDefault(); signIn(provider.id, {callbackUrl : 'http://localhost:3000/dashboard'})}}>
+                                <a href="#" className={`${provider.id}_link`} onClick={(e) => { e.preventDefault(); signIn(provider.id, {callbackUrl : `${process.env.NEXTAUTH_URL}/dashboard`})}}>
                                     <i className={`fa fa-${provider.id}`}></i> {provider.name}
                                 </a>
                             </li>
