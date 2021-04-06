fa6b5cf5e4ad14cdfd6784fd965aba516f0cd4a1
Sami Ullah <muftisamiullah@localhost.localdomain>
2021-04-06 16:16:56 +0530

    chnages in package.json

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
-        HOST:`http://13.233.125.223:8080`,
-        NEXTAUTH_URL:`http://13.126.229.6`
+        HOST:`http://127.0.0.1:8080`,
+        NEXTAUTH_URL:`http://localhost:3000`
     },
 }
         // HOST:`https://cfs-admin.herokuapp.com`
