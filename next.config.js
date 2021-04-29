ebdd386b9aea46039c71e6c00b11e79c593211c2
Sami Ullah <muftisamiullah@localhost.localdomain>
2021-04-29 18:29:20 +0530

    added basic seo functionality

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@ module.exports = {
         // HOST:`http://127.0.0.1:8080`,        
         // NEXTAUTH_URL:`http://localhost:3000`
 
-
         basePath:'https://cfs-web-prod.herokuapp.com',   // used for SEO purposes to get the basePath
         HOST:`https://cfs-admin-panel.herokuapp.com`,
-        NEXTAUTH_URL:`https://cfs-web-prod.herokuapp.com`
+        NEXTAUTH_URL:`https://cfs-web-prod.herokuapp.com`,
+        JWT_SIGNING_PRIVATE_KEY:`X_7FQqWRZXqNIWCsGYz9MfPJtsD6zj3vLTs3myNZ6cM`
     },
 }
 
