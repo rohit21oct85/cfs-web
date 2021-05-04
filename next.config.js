0e6c726b81d26e78ee8ff030ae57d57089360eed
Sami Ullah <muftisami2010gmail.com>
2021-05-04 10:36:36 +0530

    changes to package.json

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     env: {
         // basePath:'http://localhost:3000', // used for SEO purposes to get the basePath
         // HOST:`http://127.0.0.1:8080`,        
-        // NEXTAUTH_URL:`http://localhost:3000`, // only used in next.js code and not in nextauth next auth rads values from .env
+        // NEXTAUTH_URL:`http://localhost:3000`, // only used in next.js code and not in nextauth next auth reads values from .env
         // JWT_SIGNING_PRIVATE_KEY:`X_7FQqWRZXqNIWCsGYz9MfPJtsD6zj3vLTs3myNZ6cM`,
 
         basePath:'https://cfs-web-prod.herokuapp.com',   // used for SEO purposes to get the basePath
